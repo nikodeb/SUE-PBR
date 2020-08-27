c2d582f1ef9a30df9bcd8c39f8a68fd230d7f812
ndebo <n.debono@hotmail.com>
2020-08-27 14:45:49 +0200

    Removed unused code

--- datasets/__init__.py
+++ datasets/__init__.py
@@ -1,9 +1,7 @@
 from .ml_1m import ML1MDataset
-from .ml_20m import ML20MDataset
 
 DATASETS = {
-    ML1MDataset.code(): ML1MDataset,
-    ML20MDataset.code(): ML20MDataset
+    ML1MDataset.code(): ML1MDataset
 }
 
 
